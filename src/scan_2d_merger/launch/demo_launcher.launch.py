d8fc631958b0cebec604c7afab01fe6a07a3ac11
Ali Pahlevani <a.pahlevani1998@gmail.com>
2026-07-22 18:32:06 +0300

    Rebase from 'upstream'

--- src/scan_2d_merger/launch/demo_launcher.launch.py
+++ src/scan_2d_merger/launch/demo_launcher.launch.py
@@ -1,10 +1,11 @@
+from os import path
+
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
-from ament_index_python.packages import get_package_share_directory
-from launch.actions import ExecuteProcess, DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
-from os import path
 
 ARGUMENTS = [
     DeclareLaunchArgument(
